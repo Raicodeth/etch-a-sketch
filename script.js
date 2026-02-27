e59bbcad382b619aba1330f66938297dfd794c94
Raicodeth <190210161+Raicodeth@users.noreply.github.com>
2026-02-27 12:23:06 +0300

    add grid function add button add eventlistener for reset add eventlistener for grid prompt change css styling

---
+++ script.js
@@ -0,0 +1,45 @@
+const bigCont = document.querySelector(".big-container");
+
+/* Button to clear the board, ask for input to decide the number of squares, then call the function with the input passed in*/
+const btnInput = document.querySelector(".btnInput")
+btnInput.addEventListener("click", function() { 
+    bigCont.replaceChildren();
+    let userInput = prompt("Select a grid size between 16 and 100. A size 16 grid means 16x16 canvas, bigger size uses more resources.", 50);
+    amount = Math.max(16, Math.min(100, userInput))  
+    mkGrid(amount);
+})
+
+/* The function to create squares inside the bigcont*/
+
+mkGrid = function(amount) {
+let size = (960/(amount));
+for (let i = 0; i < (amount*amount); i++) {
+    let boxName = `box${i}`;
+    boxName = document.createElement("div");
+    boxName.className = "small-container";
+    boxName.style.width =  `${size}px`;
+    boxName.style.height = `${size}px`;    
+    bigCont.appendChild(boxName);
+    console.log(`${size}`)
+}
+}
+
+/* Change function*/
+const changeColor = function(e) {
+    if (e.target.className === "small-container") {
+    e.target.style.backgroundColor = "red";
+    }
+}
+
+/* Mouseover event listener*/
+const allBoxes = document.querySelectorAll(".small-container");
+bigCont.addEventListener("mouseover", changeColor);
+
+
+
+
+
+
+
+
+
